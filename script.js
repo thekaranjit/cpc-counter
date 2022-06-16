e891fcb25b54419ad816f4023e01a65a51bb5224
thekaranjit <thekaranjit@outlook.com>
2022-06-15 21:04:47 -0400

    2

--- script.js
+++ script.js
@@ -4,17 +4,3 @@ function getresultpos() {
   document.getElementById("final").innerHTML=rand;
 
 }
-
-function getresultneg() {
-
-  let rand = Math.floor(Math.random() * 100)- 90;
-  document.getElementById("final").innerHTML=rand;
-
-
-}
-
-function reset() {
-
-  document.getElementById('final').innerHTML="0";
-
-}
