af77d9bcaa99779e9e0e95e6308cfb9089a199ee
thekaranjit <thekaranjit@outlook.com>
2022-06-16 21:28:09 -0400

    1

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
-function getresultpos() {
+let timer = getElementById('stopwatch')'
 
- let rand = Math.floor(Math.random() * 100);
-  document.getElementById("final").innerHTML=rand;
+let hr = 0;
+let min = 0;
+let sec= 0;
+let stoptime = true;
+
+
+function startCounter{
+
+  if (stoptime == true) {
+    stoptime = false;
+    timerCycle();
+  }
+
+}
+
+
+function stopCounter {
+  if (stoptime == false) {
+    stoptime = true;
+  }
+}
+
+function timerCycle{
+  if stoptime == false; {
+    hr = parseInt(hr);
+    min = parseInt(min);
+    sec = parseInt(sec);
+
+    sec = sec + 1
+
+    if (sec == 60) {
+      min = min +1;
+      sec = 0;
+    }
+
+    if (min == 60) {
+      hr = hr +1;
+      min = 0;
+      sec = 0;
+    }
+
+if (sec < 10 || sec == 0) {
+  sec = '0'+ sec;
+}
+
+if (min < 10 || min == 0) {
+  min = '0'+ min;
+}
+
+if (hr < 10 || hr == 0) {
+  hr = '0'+ hr;
+}
+
+timer.innerHTML = hr + ":" + min + ":" + sec ;
+setTimeout("timerCycle()", 1000) 
+  }
+
+
+
 
 }
